818e98abd60884ce65835e854c8b811f2cd1fb66
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-20 14:53:10 +0700

    menuliskan access modifier public pada class anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -3,7 +3,16 @@
 
 class anak
 {
-    
-}
+    public:
+    string nama;
+    anak(string pNama) : nama(pNama)
+    {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
 
+    ~anak()
+    {
+        cout << "Anak \"" << nama << "\" tidak ada\n"; 
+    }
+};
 #endif
