c7508daf5e80cc5f914fbb6477b16bf3b801fed0
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-20 15:49:13 +0700

    menuliskan class manusia dan access modifiernya

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -3,5 +3,20 @@
 
 using namespace std;
 
+class manusia
+{
+    public:
+        string name;
+        jantung varJantung;
 
+        manusia(string pName) : name(pName)
+        {
+            cout << name << "hidup\n";
+        }
+
+        ~manusia(string pName) : name(pName)
+        {
+            cout << name << "mati\n";
+        }
+};
 #endif
