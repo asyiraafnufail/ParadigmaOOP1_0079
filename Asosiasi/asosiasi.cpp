cc36057ede591373104a88a1983c2fe6c924d03a
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-20 18:42:43 +0700

    menuliskan class dokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -22,3 +22,23 @@ class pasien
         void tambahDokter(dokter*);
         void cetakDokter();
 };
+
+class dokter
+{
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama)
+    {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter()
+    {
+        cout << "Dokter \"" << nama << "\" tidak ada";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
